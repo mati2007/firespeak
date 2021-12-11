f511611afe8d29f69d3623125948b34791825494
Your Namemateusz750 <2kontomateusza@gmail.com>
2021-12-11 10:18:42 +0100

    dodano formularz w pliku php, plik styli i plik java stript

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Fire Speaker - the best site ever!!!</title>
+		<link rel="stylesheet" href="style.css">
+		<script src="http://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<form>
+			<input type="text" name="username" placeholder="username min. 14 characters und 1 digit">
+			<input type="text" name="e-mail" placeholder="email">
+			<input type="text" name="password" placeholder="password min. 10 characters und 2 digits">
+			<input type="button" name ="button" value="register">
+		</form>
+	</body>
+</html>
+
+<?php
+
+
+
+
